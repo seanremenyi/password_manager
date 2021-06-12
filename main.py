df657f6645ae37b2d6d518ae565a6af3dc1c0516
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-12 20:56:47 +1000

    added checks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,10 +9,19 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0,END)
+
+    if len(website) == 0 or len(password) ==0 or len(email) ==0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
+
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\n"
+                                                              f"Password: {password}\n Is it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -23,7 +32,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=0, columnspan=2, sticky=N)
 
 
 website_label = Label(text="Website:")
