3b08d89a82bac4081b5db9e215f7ad36f61cad31
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-12 20:45:49 +1000

    UI and data store

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0,END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=0, column=1)
+
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2, sticky=W)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2, sticky=W)
+email_entry.insert(0, "fake@example.com")
+password_entry = Entry(width=33)
+password_entry.grid(row=3, column=1, sticky=W)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=1, sticky=E)
+add_button = Button(text="Add", width=30, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky=W)
+
+
+
+
+window.mainloop()
