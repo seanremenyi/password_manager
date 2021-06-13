107972ef10f9270db310a95b2e03382459c0f82c
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-13 19:46:35 +1000

    added error handling

--- main.py
+++ main.py
@@ -39,13 +39,18 @@ def save():
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
 
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(data_dict, data_file, indent=4)
+        else:
             data.update(data_dict)
 
-        with open("data.json", "w") as data_file:
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
